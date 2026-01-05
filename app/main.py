92fb35eb001827a4effdcae5bbce692fde717e06
keepgoing-228 <keepdling@gmail.com>
2026-01-05 15:47:17 +0800

    fix: order id to string type

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from app.schemas import OrderCreateInput, OrderUpdateInput
-from uuid import uuid4
 
 app = FastAPI()
 
 # simple memory storage
-orders_db = {}
+orders_db = {
+    "1": {"name": "order 1", "number": 1},
+    "2": {"name": "order 2", "number": 2},
+    "3": {"name": "order 3", "number": 3},
+}
 
 
 @app.get("/")
@@ -19,7 +22,7 @@ def get_all_orders():
 
 
 @app.get("/orders/{order_id}")
-def get_order(order_id: int):
+def get_order(order_id: str):
     """get an order by id"""
     if order_id not in orders_db:
         raise HTTPException(status_code=404, detail=f"Order {order_id} not found")
@@ -31,17 +34,18 @@ def create_order(data: OrderCreateInput):
     """create a new order"""
 
     # save the order to memory storage
-    orders_db[data.id] = {"id": data.id, "name": data.name}
+    orders_db[data.id] = {"name": data.name, "number": data.number}
 
     return {"message": "Order created successfully"}
 
 
 @app.put("/orders/{order_id}")
-def update_order(order_id: int, data: OrderUpdateInput):
+def update_order(order_id: str, data: OrderUpdateInput):
     """update an order by id"""
     if order_id not in orders_db:
         raise HTTPException(status_code=404, detail=f"Order {order_id} not found")
 
     # update the order in memory storage
     orders_db[order_id]["name"] = data.name
+    orders_db[order_id]["number"] = data.number
     return orders_db[order_id]
--- app/schemas/type.py
+++ app/schemas/type.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class OrderCreateInput(BaseModel):
-    id: int
+    id: str
     name: str
+    number: Optional[int] = None
 
 
 class OrderUpdateInput(BaseModel):
     name: str
+    number: Optional[int] = None
